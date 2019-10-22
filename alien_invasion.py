58490af9a6a5de3a7f968b20ad1e0de635579c13
LcavanzoF <cavanzo1996@gmail.com>
2019-10-22 18:19:37 -0500

    scenery and ship movements

---
+++ alien_invasion.py
@@ -0,0 +1,66 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """
+    Overall class to manage game assets and behaviour.
+    """
+
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        # Set backgroud
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+### A helper method -- does work inside a class but isn’t meant 
+###     to be called through an instance. In Python, a single 
+###     leading underscore indicates a helper method.
+    def _check_events(self):
+        """Respond to keypress and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+if __name__ == '__main__':
+    #Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+
---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """
+    A class to store all settings for Alien Invasin
+    """
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width   = 800
+        self.screen_height  = 600
+        self.bg_color       = (135, 206, 235)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+
